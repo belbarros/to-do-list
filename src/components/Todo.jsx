530080ae320283d88537e6acaa48a4d4c5573ab3
belbarros <izabelfeigel@gmail.com>
2022-09-17 18:23:51 -0300

    Added a 'finished tasks' section and the functionality to conclude a task

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import { useContext } from 'react';
 import AppContext from '../context/AppContext';
 
 export default function Todo() {
-  const { toDoList, task, setTask, setToDoList } = useContext(AppContext);
+  const { toDoList, task, setTask, setToDoList, doneList, setDoneList } = useContext(AppContext);
+  const [checked, setChecked] = useState(false);
 
   const handleInputChange = ({ target }) => {
     setTask(target.value)
@@ -14,8 +16,15 @@ export default function Todo() {
     setTask('');
   };
 
+  const handleCheckChange = ({ target }) => {
+      setDoneList([target.value, ...doneList]);
+      const newList = toDoList.filter((todo) => todo !== target.value);
+      setToDoList(newList);
+  };
+
   return (
     <div>
+      <div>
       <h1>My To-Do List ✨</h1>
       <label htmlFor='task'>
         Tarefa:
@@ -38,12 +47,23 @@ export default function Todo() {
       toDoList.map((todo) => { return (
       <p key={ todo }>
         <label>
-          <input type="checkbox" />
+          <input
+          type="checkbox"
+          value={ todo }
+          onChange={ handleCheckChange }
+          />
           { todo }
           </label>
           </p>
           )
         })
+      }
+      </div>
+      </div>
+      <div>
+      <h1>Finished Tasks ✨</h1>
+      {
+      doneList.map((done) => <p key={ done }>{ done }</p>)
       }
       </div>
     </div>
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -3,9 +3,10 @@ import AppContext from './AppContext';
 
 function Provider({ children }) {
   const [toDoList, setToDoList] = useState([]);
+  const [doneList, setDoneList] = useState([]);
   const [task, setTask] = useState('');
 
-  const contextValue = { toDoList, setToDoList,task, setTask };
+  const contextValue = { toDoList, setToDoList,task, setTask, doneList, setDoneList };
 
   return (
     <AppContext.Provider value={ contextValue }>
